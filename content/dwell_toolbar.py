aeb2424ead17c51f2b4f8ee54d0937c3e0b948f1
Roku <rokubop@gmail.com>
2024-01-16 22:44:58 -0600

    roku theme

--- content/dwell_toolbar.py
+++ content/dwell_toolbar.py
@@ -11,7 +11,7 @@ paul_tol_schemes = [
     {"background": "66CCEE", "text": "000000"},
     {"background": "228833", "text": "EEEEEE"},
     {"background": "CCBB44", "text": "000000"},
-    {"background": "EE6677", "text": "000000"}, 
+    {"background": "EE6677", "text": "000000"},
     {"background": "AA3377", "text": "EEEEEE"},
     {"background": "AAAAAA", "text": "000000"},
     {"background": "FFFFFF", "text": "000000"},
@@ -39,7 +39,7 @@ for alpha_index in range(0, 2):
         elif alpha_index == 1:
             default_colour_scheme.append({"background": scheme["background"] + "BB", "text": scheme["text"] + ""})
         elif alpha_index == 2:
-            default_colour_scheme.append({"background": scheme["background"] + "77", "text": scheme["text"] + ""})        
+            default_colour_scheme.append({"background": scheme["background"] + "77", "text": scheme["text"] + ""})
 
 def in_region(pos, x, y, width, height):
     return pos[0] >= x and pos[0] <= x + width and \
@@ -50,7 +50,7 @@ def layout_virtual_key(virtual_key, index, layout_style, alignment, grid_width, 
     if virtual_key["width"] == -1 or virtual_key["height"] == -1:
         x = 0
         y = 0
-        
+
         # The full layout uses the complete screen real estate
         if layout_style == "full":
             if alignment == "left":
@@ -65,7 +65,7 @@ def layout_virtual_key(virtual_key, index, layout_style, alignment, grid_width, 
             elif alignment == "bottom":
                 x = ( index % horizontal_amount ) * grid_width
                 y = grid_height * (vertical_amount - 1) - math.floor(index / vertical_amount) * grid_height
-        
+
         # The open layout tries to wrap around the screen
         # Tested this with 3x3, 4x4, 5x5 and 6x5 grids - Can probably be made prettier but works in all alignments as is
         elif layout_style == "open":
@@ -82,12 +82,12 @@ def layout_virtual_key(virtual_key, index, layout_style, alignment, grid_width, 
                     # Flip the direction and add the total grid offset
                     if alignment == "right":
                         x = -x + (grid_width * (horizontal_amount - 1))
-                
+
                 # Use the opposite vertical side of the screen
                 elif index >= vertical_amount + horizontal_amount - 1 and index < vertical_amount + (horizontal_amount * 2 ) - 3:
                     x = grid_width * (horizontal_amount - 1) if alignment == "left" else 0
                     y = (( index - (vertical_amount + horizontal_amount - 1) + 1) % vertical_amount ) * grid_height
-                    
+
                 # Use the bottom of the screen
                 elif index >= vertical_amount + (horizontal_amount * 2 ) - 3 and index < vertical_amount * 2 + (horizontal_amount - 2) * 2:
                     y = grid_height * (vertical_amount - 1)
@@ -95,7 +95,7 @@ def layout_virtual_key(virtual_key, index, layout_style, alignment, grid_width, 
                     # Flip the direction and add the total grid offset
                     if alignment == "right":
                         x = -x + (grid_width * (horizontal_amount - 1))
-                
+
                 # Start using the center in the same rotation
                 else:
                     new_index = index - ( vertical_amount * 2 + (horizontal_amount - 2) * 2 )
@@ -156,7 +156,7 @@ class DwellToolbarPoller:
         key_amount = len(virtual_keys)
         while key_amount > horizontal_key_amount * vertical_key_amount:
            horizontal_key_amount += 1
-    
+
         self.toolbars[id] = {
             "virtual_keys": virtual_keys,
             "dwell_ms": dwell_ms,
@@ -167,7 +167,7 @@ class DwellToolbarPoller:
             "vertical_key_amount": vertical_key_amount,
             "visible": visible
         }
-        
+
         if self.enabled and id == self.current_toolbar:
             self.update_toolbar()
 
@@ -179,7 +179,7 @@ class DwellToolbarPoller:
 
     def disable(self):
         if self.enabled:
-            self.enabled = False    
+            self.enabled = False
             self.content.publish_event("screen_regions", "dwell_toolbar", "remove")
             self.content.publish_event("cursor_regions", "toolbar_icon", "remove")
             self.selected_index = -1
@@ -202,7 +202,7 @@ class DwellToolbarPoller:
             self.toolbar_index = -1
             self.toolbar_dwell_ms = 0
             self.toolbar_select_threshold_ms = self.toolbars[self.current_toolbar]["dwell_ms"]
-            
+
             # Determine the base layout parameters
             screens = ui.screens()
             if self.monitor_index >= len(screens):
@@ -213,20 +213,20 @@ class DwellToolbarPoller:
             alignment = self.toolbars[self.current_toolbar]["alignment"]
             grid_width = int(round(screens[self.monitor_index].rect.width / horizontal_amount))
             grid_height = int(round(screens[self.monitor_index].rect.height / vertical_amount))
-            
+
             screen_regions = []
             toolbar_items = []
             layout_index = 0
             colour_index = 0
-            
+
             # Layout all the virtual keys
             for virtual_key in self.toolbars[self.current_toolbar]["virtual_keys"]:
                 virtual_key["rect"] = layout_virtual_key(virtual_key, layout_index, layout_style, alignment, grid_width, grid_height, horizontal_amount, vertical_amount)
                 virtual_key["rect"].x += screens[self.monitor_index].rect.x
-                virtual_key["rect"].y += screens[self.monitor_index].rect.y                
+                virtual_key["rect"].y += screens[self.monitor_index].rect.y
                 if virtual_key["width"] == -1 or virtual_key["height"] == -1:
                     layout_index += 1
-                
+
                 # Determine a colour
                 background_colour = ''
                 text_colour = ''
@@ -239,7 +239,7 @@ class DwellToolbarPoller:
                     text_colour = virtual_key["text_colour"] if "text_colour" in virtual_key and virtual_key["text_colour"] else ''
                 virtual_key["colour"] = background_colour
                 virtual_key["text_colour"] = text_colour
-                
+
                 # Create the screen region content
                 if self.toolbars[self.current_toolbar]["visible"]:
                     screen_region = self.content.create_screen_region("dwell_toolbar", background_colour, \
@@ -248,12 +248,12 @@ class DwellToolbarPoller:
                         virtual_key["relative_y"] if "relative_y" in virtual_key else 0)
                     if text_colour:
                         screen_region.text_colour = text_colour
-                
+
                     screen_regions.append(screen_region)
-                
+
                 # Add the virtual key to the active toolbar items
                 toolbar_items.append(virtual_key)
-            
+
             self.content.publish_event("screen_regions", "dwell_toolbar", "replace", screen_regions)
             self.toolbar_items = toolbar_items
 
@@ -268,14 +268,14 @@ class DwellToolbarPoller:
                 self.toolbar_dwell_ms += self.check_interval_ms
             else:
                 self.toolbar_index = -1
-        
+
         if self.toolbar_index == -1:
             self.toolbar_dwell_ms = 0
             for index, toolbar_item in enumerate(self.toolbar_items):
                 if in_region(pos, toolbar_item["rect"].x, toolbar_item["rect"].y, toolbar_item["rect"].width, toolbar_item["rect"].height):
                     self.toolbar_index = index
                     break
-        
+
         if self.toolbar_index > -1 and self.toolbar_dwell_ms >= select_threshold:
             self.select_cursor(self.toolbar_index)
 
@@ -290,11 +290,11 @@ class DwellToolbarPoller:
         toolbar_item = self.toolbar_items[self.selected_index] if self.selected_index > -1 and self.selected_index < len(self.toolbar_items) else None
         if toolbar_item is not None:
             toolbar_item["callback"]()
-    
+
     def clear_cursor(self):
         self.content.publish_event("cursor_regions", "toolbar_icon", "remove")
         self.selected_index = -1
-        
+
     def set_visibility(self, visible: bool):
         if self.current_toolbar is not None and self.current_toolbar in self.toolbars:
             self.toolbars[self.current_toolbar]["visible"] = visible
@@ -315,12 +315,12 @@ class Actions:
         # Make sure something is visible if no text is given
         if text == '' and icon == '':
            text = '  '
-           
+
         # If the action is a string, we default to it being a keypress
         callback = action
         if isinstance(action, str):
             callback = lambda action=action: actions.key(action)
-        
+
         return {
             "callback": callback,
             "text": text,
--- hud_keys.talon
+++ hud_keys.talon
@@ -1,4 +1,4 @@
 tag: user.talon_hud_available
 tag: user.talon_hud_visible
 -
-key(alt-shift-end): user.hud_toggle_focus()
+# key(alt-shift-end): user.hud_toggle_focus()
